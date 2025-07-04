8f559ec70d183ca52dd6476e31579040fc1ef48a
Rohit chauhan <75549998+Rohiitt012@users.noreply.github.com>
2025-07-04 21:27:53 +0000

    array tak baat chali gyi

--- part1.js
+++ part1.js
@@ -1,1 +1,64 @@
-console.log("hello rohit");
+// console.log("hello rohit");
+// let name="rohit";
+
+// let age = "24";
+
+// console.log(`hello my name ${name} and my age is ${age}`);
+// const gameName = String("rohit chauhan");
+// console.log(gameName);
+// // console.log(typeof gameName);
+// const gameName2 = new String("diksha chauhan");
+// console.log(gameName2[5]); // ye hai key value access
+// console.log(gameName2[6]);
+// console.log(gameName2.toUpperCase());
+// console.log(gameName2.charAt(4));
+// console.log(gameName2.substring(0 ,4));
+// const newgameName = new String("     rohit  h   ");
+// console.log(newgameName.trimEnd().trimStart());
+// console.log(newgameName);
+
+// const url = "google.com";
+// // url.replace("go","to");
+// console.log(url.replace('go','to'));
+// const firstName = new String("rohit chauhan");
+// console.log(firstName.includes("roi"));
+// console.log(firstName.split(" "));
+
+
+// let name = "rohit";
+// let lastName = "chauhan";
+// console.log(`my name is ${name} ${lastName}`);
+
+// // let score = new Number(400);
+// // console.log(score);
+// // console.log(typeof score);
+// // const newBalance = new Number(445);
+// // console.log(newBalance.valueof(555));
+// const min = 50;
+// const max = 100;
+// // console.log(Math.round());
+// // console.log(Math.random()*10 + max-min);
+// console.log(Math.floor(Math.random()*10 + max-min));
+// let dateNow = new Date();
+// // console.log(dateNow.toDateString());
+// // console.log(dateNow.toISOString());
+// // console.log(dateNow.toJSON());
+// // console.log(dateNow.toLocaleString());
+// let myArray1 = [0,1,25,3,4454,5];
+// let myArray = new Array(45,12,0,12,45,56);
+// // console.log(myArray);
+// // console.log(myArray1);
+// // myArray1.unshift(100);
+// // myArray1.pop();
+// // myArray1.shift();
+// // myArray1.push(1212);
+// // console.log(myArray);
+// // console.log(myArray1);
+// console.log(myArray1[2]);
+
+let myArray = new Array(0,1,2,3,4);
+console.log("A", myArray);
+console.log("B",myArray.slice(0,3));
+console.log("A", myArray);
+console.log("c", myArray.splice(0,3));
+console.log("A", myArray);

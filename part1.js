330b6ca9d5d4f07cf842b6371a72a835769f255c
Rohit chauhan <75549998+Rohiitt012@users.noreply.github.com>
2025-07-08 21:26:54 +0000

    ss funv

--- part1.js
+++ part1.js
@@ -56,9 +56,71 @@
 // // console.log(myArray1);
 // console.log(myArray1[2]);
 
-let myArray = new Array(0,1,2,3,4);
-console.log("A", myArray);
-console.log("B",myArray.slice(0,3));
-console.log("A", myArray);
-console.log("c", myArray.splice(0,3));
-console.log("A", myArray);
+// let myArray = new Array(0,1,2,3,4);
+// console.log("A", myArray);
+// console.log("B",myArray.slice(0,3));
+// console.log("A", myArray);
+// console.log("c", myArray.splice(0,3));
+// console.log("A", myArray);
+
+
+// function userOne(a,b){ // a and b is parameter
+//     return a+b;   // jab function return kr de value uske bad kuch vi print ni hota function me
+//     console.log("rohit chauhan");
+
+// }
+// console.log(userOne(3,5));  // 3 and 5 is argument 
+// function userTwo(anyArray){
+// console.log("this is array");
+// }
+// userTwo([10,20,30]);
+
+
+// function loginUser(userName){if(userName===undefined){
+// console.log("please entr the code")
+// }
+//     return `${userName} just loggin`
+// };
+// console.log(loginUser());
+
+
+// function calculateCartPrice(...numbers){ // rest...and sprits operator 
+//     return numbers;
+// };
+// console.log(calculateCartPrice(12,14,16,18));  
+// // out in form of array [12,14,16,18]
+// function calculateCartPrice1(val1, val2, ...numbers){
+//     return numbers;
+// };
+// console.log(calculateCartPrice1(12,14,16,18));
+// // output is 16 and 18 
+
+// const user1={
+//     name: "Rohit chauhan",
+//     age: 29
+// };
+
+// const user2={
+//     name: "Rahul cahuhan",
+//     age: 32
+// };
+
+// function handleObject(user){     // ye hai function defination
+//     return user;
+// }
+// console.log(handleObject(user1));  // ye hai function calling
+// console.log(handleObject(user2));  // ye hai function calling
+// console.log(handleObject({
+//     username:"rohan",
+//     age:44     // direct object vi pass kr skte hai function me
+// }));  // ye hai function calling
+// console.log(handleObject([]));  // ye hai function calling
+// output in form of object{name:"Rohit chauhan", age:29}  
+// types check krna pdta hai 
+
+
+const myArray=[200,400,600,800];
+function secondValue (anyArray){
+    return anyArray[2];
+};
+console.log(secondValue([200,55,44])); //direct array 
